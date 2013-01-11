adc7f2c391773de618e93512c4304a7b3795b90f
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-11 21:41:55 +0100

    Add query logging.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,13 @@
 package controllers
 
+import play._
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index(q: String) = Action {
+    Logger.info("Query = " + q)
+
     q match {
       case "Quelle est ton adresse email" => Ok("o.jacquemart@gmail.com")
       case _ => Ok("")
