fd9f539f7082d46d2034cc9551185ed4dc9534b6
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-12 15:46:54 +0100

    Log request body.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,23 +5,24 @@ import play.api.mvc._
 
 object Application extends Controller {
 
+  def debug() = Action {
+    Ok(views.html.index())
+  }
+
   def index(q: String) = Action {
     Logger.info("Query = " + q)
 
     q match {
       case "Quelle est ton adresse email" => Ok("o.jacquemart@gmail.com")
-      case "Es tu abonne a la mailing list(OUI/NON)" => Ok("OUI")
-      case "Es tu heureux de participer(OUI/NON)" => Ok("OUI")
-      case "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)" => Ok("OUI")
       case "Est ce que tu reponds toujours oui(OUI/NON)" => Ok("NON")
       case "As tu bien recu le premier enonce(OUI/NON)" => Ok("NON")
-      case _ => Ok("")
+      case _ => Ok("OUI")
     }
   }
 
    def enonce(index: Int) = Action { request =>
-    Logger.info("Request = " + request)
-    Ok("")
-   }
+    println(request.body)
+     Ok
+  }
 
 }
