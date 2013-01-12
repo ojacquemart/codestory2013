c0c019e50814640d58ff22e2c475e7a4fac70cb4
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-12 11:16:04 +0100

    Change answer.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,7 +13,7 @@ object Application extends Controller {
       case "Es tu abonne a la mailing list(OUI/NON)" => Ok("OUI")
       case "Es tu heureux de participer(OUI/NON)" => Ok("OUI")
       case "Es tu pret a recevoir une enonce au format markdown par http post(OUI/NON)" => Ok("OUI")
-      case "Est ce que tu reponds toujours oui(OUI/NON)" => Ok("OUI")
+      case "Est ce que tu reponds toujours oui(OUI/NON)" => Ok("NON")
       case _ => Ok("")
     }
   }
