d70eb747c05384159482a6340d5e0e24613facab
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-13 22:03:02 +0100

    Fix compile error.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,14 +6,12 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index(q: String) = Action {
-    Logger.info("Query = "   q)
+    Logger.info("Query = " + q)
 
     q match {
       case "Quelle est ton adresse email" => Ok("o.jacquemart@gmail.com")
-      case "Es tu abonne a la mailing list(OUI/NON)" => Ok("o.jacquemart@gmail.com")
+      case "Es tu abonne a la mailing list(OUI/NON)" => Ok("NON")
       case "Est ce que tu reponds toujours oui(OUI/NON)" => Ok("NON")
-      case "Est ce que tu reponds toujours oui(OUI/NON)" => Ok("NON")
-      case "As tu bien recu le premier enonce(OUI/NON)" => Ok("OUI")
       case _ => Ok("OUI")
     }
   }
