54e49341726bcfd174c99c92609cf28eebe9a0a4
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-01-25 21:32:48 +0100

    Add controller to check request from bot.

---
+++ app/controllers/SimpleApplication.java
@@ -0,0 +1,19 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+import play.data.validation.Constraints.*;
+
+import java.util.*;
+
+import views.html.*;
+
+public class SimpleApplication extends Controller {
+
+      public static Result javajaja() {
+    	System.out.println(request().body());
+        return ok("");
+    }
+
+}
