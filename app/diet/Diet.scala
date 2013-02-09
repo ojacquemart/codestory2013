a21fd742f772482104980749b2c0c481f7b47fea
Olivier Jacquemart <o.jacquemart@gmail.com>
2013-02-10 00:48:03 +0100

    Add first diet implementation.

---
+++ app/diet/Diet.scala
@@ -0,0 +1,55 @@
+package diet
+
+import collection.mutable.ListBuffer
+
+import play.api.libs.json._
+
+case class Activity(name: String, value: Int)
+
+case class Diet(activities: List[Activity]) {
+
+  def resolve(): Set[List[String]] = {
+    var solutions: ListBuffer[List[String]] = ListBuffer()
+
+    activities.foreach({
+      a => {
+        val nexts = getNexts(a)
+        val sizeNexts = nexts.size
+        for (i <- 0 to sizeNexts) {
+          val comb = nexts.takeRight(sizeNexts - i)
+          if (comb.map(_.value).sum + a.value == 0) {
+            val combAndCurrent = comb ++ List(a)
+            solutions += combAndCurrent.map(_.name).sorted
+          }
+        }
+      }
+    })
+
+    solutions.toSet
+  }
+
+  def getNexts(activity: Activity) = activities.filterNot(_.name == activity.name)
+
+}
+
+
+object Diet {
+
+  def apply(json: String): JsValue = {
+    val result = resolve(json)
+    if (result.isEmpty) Json.toJson(List("no solution"))
+    else Json.toJson(result.head)
+  }
+
+  def resolve(json: String) = {
+    val activities = jsonAsActivites(json)
+    new Diet(activities).resolve
+  }
+
+  def jsonAsActivites(json: String): List[Activity] = {
+    val jsObjects: List[JsObject] = Json.parse(json).as[List[JsObject]]
+    jsObjects.map(j => new Activity((j \ "name").as[String], (j \ "value").as[Int]))
+  }
+
+
+}
---
+++ test/DietSpec.scala
@@ -0,0 +1,51 @@
+import org.specs2.mutable._
+
+import diet._
+
+class DietSpec extends Specification {
+
+  "The DietSpec" should {
+
+    "read json" in {
+      val json =
+        """[
+          { "name" : "croissant", "value" : 180 },
+          { "name" : "au-travail-a-velo", "value" : -113 },
+          { "name" : "coca-light", "value" : 1 },
+          { "name" : "guitar-hero", "value" : -181 }
+        ]"""
+
+      Diet.jsonAsActivites(json) must equalTo(List(
+        new Activity("croissant", 180),
+        new Activity("au-travail-a-velo", -113),
+        new Activity("coca-light", 1),
+        new Activity("guitar-hero", -181)))
+    }
+
+    "resolve diet with first question" in {
+      val json =
+        """[
+          { "name" : "croissant", "value" : 180 },
+          { "name" : "au-travail-a-velo", "value" : -113 },
+          { "name" : "coca-light", "value" : 1 },
+          { "name" : "guitar-hero", "value" : -181 }
+        ]"""
+
+     val result = Diet(json).toString
+      result must be equalTo("""["coca-light","croissant","guitar-hero"]""")
+    }
+
+    "resolve diet with no answer" in {
+      val json =
+        """[
+          { "name" : "croissant", "value" : 180 }
+        ]"""
+
+     val result = Diet(json).toString
+      result must be equalTo("""["no solution"]""")
+    }
+
+  }
+
+
+}
